ff73a1f3b0c95280dfdd0df25325f75aaaf4b43c
SainyGb <83477876+SainyGb@users.noreply.github.com>
2023-06-24 21:49:50 -0300

    Finalized Basic Inteligent Agent

--- base_dinamica_posicao_objetos.pl
+++ base_dinamica_posicao_objetos.pl
@@ -1,17 +1,35 @@
 :- dynamic pos/2.
+:- dynamic mochila_robo/1.
 
 pos(tv, sala).
 pos(bola,quarto).
 pos(carteira, quarto).
 pos(chave, garagem).
-pos(robo, garabem).
+pos(robo, garagem).
+
+mochila:-   findall(X, mochila_robo(X), Result),
+            format("~w", [Result]).       
+
+pegue(Objeto):- Objeto \= robo,
+                pos(Objeto, Local),
+    			pos(robo, Local),
+                asserta(mochila_robo(Objeto)),
+    			retract(pos(Objeto, Local)),
+                format("Robo pegou ~w e colocou na mochila.", [Objeto]).
+
+solte(Objeto):- pos(robo, Local),
+                mochila_robo(Objeto),
+                retract(mochila_robo(Objeto)),
+                asserta(pos(Objeto, Local)),
+                format("Robo soltou ~w na ~w.", [Objeto, Local]).
 
 ande(Destino) :- pos(_, Destino),
                  retract(pos(robo, Origem)),
                  asserta(pos(robo, Destino)),
                  format('Robo andou de ~w para ~w', [Origem, Destino]).
 
-estou :- listing(pos(robo,_)).
+estou :- pos(robo,X), format("Local atual do robo: ~w", [X]), !.
 
-objetos :- findall(Local, pos(robo, Local), Result),
-           format("Objetos: ~w", [Result]).
+objetos :-  pos(robo,Local),
+            findall(Objetos, (pos(Objetos, Local), Objetos \= robo), Result),
+            format("Objetos: ~w", [Result]), !.
